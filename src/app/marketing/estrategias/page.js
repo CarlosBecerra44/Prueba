144696214210a27aae21f25ddfd5dd2552bcac79
CarlosBecerra44 <138605506+CarlosBecerra44@users.noreply.github.com>
2024-09-06 08:37:09 -0600

    tabla

---
+++ src/app/marketing/estrategias/page.js
@@ -0,0 +1,12 @@
+import { TablaEventosMejorada as Ta } from "@/components/component/tabla-eventos-mejorada";
+function page() {
+    
+      return (
+        <div>
+          
+          <Ta />
+        </div>
+      );
+}
+
+export default page;
