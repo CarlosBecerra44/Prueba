a8d80615f1ce0571f90f1e30f09f7f80418aee74
GusCardenas7 <guscardenas63@gmail.com>
2024-10-14 16:06:10 -0600

    Correccion de errores segundo archivo

--- src/app/permisos/ver_permisos/page.js
+++ src/app/permisos/ver_permisos/page.js
@@ -1,9 +1,12 @@
 import { VisualizarPermiso as VerPermiso } from "@/components/component/ver_permisos";
+import { Suspense } from 'react';
 function page() {
       return (
+            <Suspense>
         <div>
           <VerPermiso />
         </div>
+            </Suspense>
       );
 }
 
