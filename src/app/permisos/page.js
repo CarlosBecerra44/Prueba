148a7d6914488672af3141911803b5202618915b
GusCardenas7 <guscardenas63@gmail.com>
2024-10-14 16:05:12 -0600

    Correccion de errores

--- src/app/permisos/page.js
+++ src/app/permisos/page.js
@@ -1,11 +1,14 @@
 import { TablaPermisos as Pe } from "@/components/component/tabla_permisos_individuales";
+import { Suspense } from 'react';
 function page() {
     
       return (
+          <Suspense>
         <div>
           
           <Pe />
         </div>
+    </Suspense>
       );
 }
 
