fde47402681f4878b6f03b32345d1b7a143e6159
GusCardenas7 <guscardenas63@gmail.com>
2024-11-04 14:10:41 -0600

    actualizacion login nuevos cambios

--- src/components/component/login.jsx
+++ src/components/component/login.jsx
@@ -49,22 +49,6 @@ password,
           <img src="/logo.png" alt="" />
         </div>
         <form onSubmit={handleSubmit} className="space-y-4">
-          <Button
-            onClick={() => signIn("google", { callbackUrl: "/inicio" })}
-            variant="outline"
-            className="w-full"
-          >
-            <ChromeIcon className="mr-2 h-4 w-4" />
-            Iniciar sesión con Google
-          </Button>
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">Continúa con correo</span>
-            </div>
-          </div>
           <div className="space-y-2">
             <Label htmlFor="email">Correo electrónico</Label>
             <Input
@@ -90,7 +74,7 @@ password,
           {error && <p className="text-center text-red-500">{error}</p>}
           <Button type="submit" className="w-full">Iniciar sesión</Button>
         </form>
-        <div className="text-center text-sm text-muted-foreground">
+        <div className="text-center text-sm text-muted-foreground" hidden>
           ¿Aún no tienes una cuenta?{" "}
           <Link href="/login/registro" className="underline">Regístrate</Link>
         </div>
