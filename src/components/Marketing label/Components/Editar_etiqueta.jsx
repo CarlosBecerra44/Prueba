0fac00c7d7a0b4e5a5b403295dae36a102f9d9de
GusCardenas7 <guscardenas63@gmail.com>
2025-01-06 16:49:13 -0600

    Update Editar_etiqueta.jsx

--- src/components/Marketing label/Components/Editar_etiqueta.jsx
+++ src/components/Marketing label/Components/Editar_etiqueta.jsx
@@ -733,7 +733,7 @@ export function EditarEtiqueta() {
       <Card>
         <CardHeader>
           <CardTitle>Ingeniería de Productos</CardTitle>
-          <CardDescription>Jania Leyva o Roger Castellanos</CardDescription>
+          <CardDescription>Jania Leyva, Roger Castellanos o Emanuel Moya</CardDescription>
         </CardHeader>
         <CardContent>
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-6">
