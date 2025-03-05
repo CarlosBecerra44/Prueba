973f7ec67553222f493c4c46d6532d1bcc7a2a75
GusCardenas7 <guscardenas63@gmail.com>
2025-03-05 16:00:49 -0600

    Update sequelize.js

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -3,6 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("aionnet_productivo", "aionnet", "$ZkSex&+PSbQ", {
   host: "50.6.199.166", // Cambia esto según tu servidor
   dialect: "mysql", // Cambia a "mysql", "sqlite" o "mssql" según tu BD
+  dialectModule: require("mysql2"),
 });
 
 (async () => {
