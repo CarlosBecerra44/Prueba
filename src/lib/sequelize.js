4a84b689944dda5ea5acdd4a61208fafd1d3e24e
GusCardenas7 <guscardenas63@gmail.com>
2025-03-06 14:36:05 -0600

    conexiones con produccion

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("prueba", "root", "", {
-  host: "localhost", // Cambia esto según tu servidor
+const sequelize = new Sequelize("aionnet_productivo", "aionnet", "$ZkSex&+PSbQ", {
+  host: "50.6.199.166", // Cambia esto según tu servidor
   dialect: "mysql", // Cambia a "mysql", "sqlite" o "mssql" según tu BD
   dialectModule: require("mysql2"),
 });
