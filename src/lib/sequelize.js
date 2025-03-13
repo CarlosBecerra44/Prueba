c1a9c62a216acc394a4886a4131fe2151d33bbf5
GusCardenas7 <guscardenas63@gmail.com>
2025-03-13 16:37:03 -0600

    conexiones prueba

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("aionnet_productivo", "aionnet", "Rrio1003", {
-  host: "50.6.199.166", // Cambia esto según tu servidor
+const sequelize = new Sequelize("prueba", "root", "", {
+  host: "localhost", // Cambia esto según tu servidor
   dialect: "mysql", // Cambia a "mysql", "sqlite" o "mssql" según tu BD
   dialectModule: require("mysql2"),
 });
