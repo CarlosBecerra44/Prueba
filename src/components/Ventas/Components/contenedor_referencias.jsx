884e6e835f712c5afeaedecb6efffe3cd3d7a7ee
jesuslim <limchuy7@gmail.com>
2025-05-23 17:01:05 -0600

    feat: contenedor de referencias

--- src/components/Ventas/Components/contenedor_referencias.jsx
+++ src/components/Ventas/Components/contenedor_referencias.jsx
@@ -1,5 +1,6 @@
 "use client";
 import axios from "axios";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 export default function ContenedorReferencias(props) {
@@ -9,14 +10,31 @@ export default function ContenedorReferencias(props) {
   useEffect(() => {
     const fetchReferencias = async () => {
       await axios.get(`/api/Sales/getReferencias?id=${id}`).then((res) => {
-        console.log({ ref: res.data.referencias });
-        setReferencias(res.data.referencias);
+        const referenciasConImagenes = res.data.referencias.map((ref) => {
+          const imagenes = [ref.img1, ref.img2, ref.img3, ref.img4]
+            .filter(Boolean)
+            .map((img) => ({
+              id: crypto.randomUUID(),
+              file: img,
+            }));
+
+          return {
+            id: ref.id || "",
+            nombre: ref.nombre || "",
+            link: ref.link || "",
+            notas: ref.notas || "",
+            tipo: ref.tipo || "",
+            imagenes,
+          };
+        });
+        setReferencias(referenciasConImagenes);
       });
     };
 
     if (id) fetchReferencias();
   }, [id]);
   return (
+    // TODO: Agregar estilos al contenedor
     <fieldset className="border border-gray-300 p-4 rounded-lg">
       <legend className="text-lg font-semibold mx-2 flex gap-2">
         Referencias
@@ -30,6 +48,27 @@ export default function ContenedorReferencias(props) {
           <p className="text-sm">{referencia.link}</p>
           <p className="text-sm">{referencia.notas}</p>
           <p className="text-sm">{referencia.tipo}</p>
+          <div className="flex gap-2">
+            {referencia.imagenes.map((imagen) => (
+              <Image
+                key={imagen.id}
+                src={`/api/Sales/obtenerImagenesReferencias?rutaImagen=${encodeURIComponent(
+                  imagen.file
+                )}`}
+                width={150}
+                height={150}
+                alt={referencia.nombre}
+              />
+            ))}
+          </div>
+          {/* <Image
+            src={`/api/Sales/obtenerImagenesReferencias?rutaImagen=${encodeURIComponent(
+              referencia.img1
+            )}`}
+            width={100}
+            height={100}
+            alt={referencia.nombre}
+          /> */}
         </div>
       ))}
     </fieldset>
