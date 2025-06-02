0acc182a351712f4cc5034869d49c4ec7ef13843
jesuslim <limchuy7@gmail.com>
2025-06-02 13:30:06 -0600

    feat: adecuacion al navbar para que responda al tamaño de la  pantalla

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,78 +1,98 @@
-"use client"
+"use client";
 import { Inter } from "next/font/google";
-import { usePathname } from 'next/navigation'; // Importa usePathname
-import "./globals.css";
-import { Navbarv1 as Inicio } from "@/components/navbar";
 import { SessionProvider } from "next-auth/react";
+import { usePathname } from "next/navigation"; // Importa usePathname
 const inter = Inter({ subsets: ["latin"] });
+import { Button } from "@mui/material";
+import { Navbarv1 as Inicio } from "@/components/navbar";
+import "./globals.css";
 
 import NotificationBell from "@/components/Reminder/Components/notificationBell";
 import { Label } from "recharts";
+import { Suspense, useEffect, useState } from "react";
+import { Menu } from "lucide-react";
 /*export const metadata = {
   title: "AIONET",
 };*/
 
-export default function RootLayout({ children}) {
+export default function RootLayout({ children }) {
   const pathname = usePathname(); // Obtiene la ruta actual
-  const showNavbar = pathname !== '/login';
-  const showNavbar3 = pathname !== '/'; // Determina si debe mostrarse la Navbar
-  const showNavbar2 = pathname !== '/login/registro'; // Determina si debe mostrarse la Navbar
+  const showNavbar = pathname !== "/login";
+  const showNavbar3 = pathname !== "/"; // Determina si debe mostrarse la Navbar
+  const showNavbar2 = pathname !== "/login/registro"; // Determina si debe mostrarse la Navbar
+  const [isOpen, setIsOpen] = useState(true);
+  const [mounted, setMounted] = useState(false);
 
-   
-    
+  useEffect(() => {
+    const screenWidth = window.innerWidth;
+    setIsOpen(screenWidth >= 768);
+    setMounted(true);
+  }, []);
+
+  // if (!mounted) return null;
+
   return (
-     <html lang="en">
-    {/* <head>
+    <html lang="en" suppressHydrationWarning={true}>
+      {/* <head>
       <title>AIONET</title>
       <meta name="description" content="Bienvenido a AIONET, la plataforma ..." />
       <link rel="icon" type="image/png" sizes="16x16" href="/logo1.png" />
     </head> */}
-    <body suppressHydrationWarning={true} className={inter.className}>
-    <SessionProvider> 
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-  {/* Navbar */}
-  {showNavbar && showNavbar3 && showNavbar2 && (
-    <div
-      style={{
-        width: '250px',
-        minWidth: '250px',
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        height: '100vh',
-        backgroundColor: '#000000d6',
-        zIndex: 1, // Para que se superponga al contenido en pantallas pequeñas
-      }}
-    >
-      <Inicio />
-    </div>
-  )}
+      <body suppressHydrationWarning={true} className={inter.className}>
+        <SessionProvider>
+          <div style={{ display: "flex" }}>
+            {/* Navbar */}
+            {showNavbar && showNavbar3 && showNavbar2 && (
+              <div className="flex">
+                <div
+                  style={{
+                    position: `${
+                      window.innerWidth >= 768 ? "relative" : "absolute"
+                    }`,
+                    top: "0",
+                    left: "0",
+                    height: "100vh",
+                    backgroundColor: "#000000d6",
+                    zIndex: 1, // Para que se superponga al contenido en pantallas pequeñas
+                  }}
+                  className={isOpen ? "md:block h-screen" : "hidden "}
+                >
+                  <Suspense>
+                    <Inicio />
+                  </Suspense>
+                </div>
+              </div>
+            )}
 
-  {/* Contenido Principal */}
-  <div style={{ marginLeft: '250px', width: 'calc(100% - 250px)', padding: '1rem', overflowX: 'auto', boxSizing: 'border-box' }}>
-  {showNavbar && showNavbar3 && showNavbar2 && (
-  <header
-        style={{
-          display: "flex",
-          justifyContent: "flex-end", // Alinea los elementos al extremo derecho
-          alignItems: "center",
-          height: "60px", // Altura fija para el encabezado
-          padding: "0 20px", // Espaciado interno
-          backgroundColor: "rgb(248 249 250 / 0%)", // Fondo claro
-        }}
-      >
-        <NotificationBell />
-      </header> )}
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-        {children}
-    </div>
-</div>
-</div>
-
-      
-  </SessionProvider>
-    </body>
-  </html>
-
+            {/* Contenido Principal */}
+            <div
+              style={{
+                padding: "1rem",
+                overflowX: "auto",
+              }}
+              className="w-full"
+            >
+              {showNavbar && showNavbar3 && showNavbar2 && (
+                <header className="flex flex-col items-end w-full gap-2">
+                  <div
+                    onClick={() => setIsOpen(!isOpen)}
+                    className={
+                      isOpen ? "md:hidden cursor-pointer z-50" : "md:hidden"
+                    }
+                  >
+                    <Menu
+                      className="hover:text-slate-200"
+                      style={{ color: "#333" }}
+                    />
+                  </div>
+                  <NotificationBell />
+                </header>
+              )}
+              <div className="mx-auto">{children}</div>
+            </div>
+          </div>
+        </SessionProvider>
+      </body>
+    </html>
   );
 }
--- src/components/Reminder/Components/notificationBell.jsx
+++ src/components/Reminder/Components/notificationBell.jsx
@@ -17,10 +17,13 @@ export function NotificationBell() {
     const fetchUserData = async () => {
       const session = await getSession();
       if (session) {
-        const response = await fetch('/api/Users/getUser', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ correo: session.user.email, numero_empleado: session.user.numero_empleado }),
+        const response = await fetch("/api/Users/getUser", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            correo: session.user.email,
+            numero_empleado: session.user.numero_empleado,
+          }),
         });
         const userData = await response.json();
         if (userData.success) {
@@ -98,9 +101,6 @@ export function NotificationBell() {
       <button
         onClick={() => setIsModalOpen((prev) => !prev)}
         style={{
-          position: "fixed",
-          top: "15px",
-          right: "15px",
           background: "transparent",
           border: "none",
           cursor: "pointer",
@@ -113,7 +113,7 @@ export function NotificationBell() {
         {hasNotifications && (
           <div
             style={{
-              position: "absolute",
+              position: "relative",
               top: "-3px",
               right: "-3px",
               background: "red",
@@ -137,8 +137,6 @@ export function NotificationBell() {
         <div
           style={{
             position: "fixed",
-            top: "60px",
-            right: "60px",
             width: "320px",
             maxHeight: "450px",
             background: "white",
