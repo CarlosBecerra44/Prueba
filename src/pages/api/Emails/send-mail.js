ea899c7eae75d2588c1e7e8fb795b39ed22c79f9
GusCardenas7 <guscardenas63@gmail.com>
2025-06-11 10:25:49 -0600

    Correccion envio de correos etiquetas
    Se optimizó el envio de correos cuando se crea una nueva etiqueta.

--- src/pages/api/Emails/send-mail.js
+++ src/pages/api/Emails/send-mail.js
@@ -1,31 +1,33 @@
-
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { emails, subject, message } = req.body;
 
-    // Configurar el transporte SMTP utilizando los detalles de cPanel
     const transporter = nodemailer.createTransport({
-      host: 'mail.aionnet.net', // Servidor SMTP de tu dominio
-      port: 465, // O 587 si estás utilizando TLS
-      secure: true, // Usar true para puerto 465, false para otros puertos
+      host: 'mail.aionnet.net',
+      port: 465,
+      secure: true,
       auth: {
-        user: 'etiquetas@aionnet.net', // Tu correo completo
-        pass: '!SD[ftw.ZOR4', // La contraseña de tu cuenta de correo
+        user: 'etiquetas@aionnet.net',
+        pass: '!SD[ftw.ZOR4',
       },
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
     });
 
-    // Enviar correos a cada dirección
     try {
-      for (const email of emails) {
-        await transporter.sendMail({
+      const sendMailPromises = emails.map(email =>
+        transporter.sendMail({
           from: 'etiquetas@aionnet.net',
           to: email,
-          subject: subject,
+          subject,
           text: message,
-        });
-      }
+        })
+      );
+
+      await Promise.all(sendMailPromises);
 
       res.status(200).json({ message: 'Correos enviados exitosamente' });
     } catch (error) {
