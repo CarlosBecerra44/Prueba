5057a1b2cf8bff2c3151f3d5949e779240c6c841
GusCardenas7 <guscardenas63@gmail.com>
2025-08-03 15:05:17 -0600

    Archivos para la base de datos de produccion
    Se cambiaron las conexiones para productivo

--- src/middleware.js
+++ src/middleware.js
@@ -32,7 +32,7 @@ export async function middleware(req) {
   let permisos = {};
   try {
     const res = await fetch(
-      `https://aionnetpruebas.vercel.app/api/MarketingLabel/permiso?userId=${idUser}`
+      `https://aionnet.vercel.app/api/MarketingLabel/permiso?userId=${idUser}`
     );
     if (res.ok) {
       permisos = await res.json();
