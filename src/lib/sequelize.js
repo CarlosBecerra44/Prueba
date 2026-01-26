2c49982aaef0e0f7f9d4a91bc26bc447170ea0ea
omararaujo2022 <jair.orozco2000@gmail.com>
2026-01-26 10:08:44 -0600

    Hotfix filtro de Eliminados y bug de registro sin correo
    Se elimino el filtro de "Eliminados" en el apartado de firmas y  se soluciono el bug de registro que no dejaba crear usuarios nuevos sin correo.

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -1,21 +1,21 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("aionnet_productivo", "aionnet", "Rrio1003", {
-  host: "50.6.199.166", // Cambia esto según tu servidor
-  dialect: "mysql", // Cambia a "mysql", "sqlite" o "mssql" según tu BD
-  dialectModule: require("mysql2"),
+	host: "50.6.199.166", // Cambia esto según tu servidor
+	dialect: "mysql", // Cambia a "mysql", "sqlite" o "mssql" según tu BD
+	dialectModule: require("mysql2"),
 });
 
 // bases de datos
 // "aionnet_pruebas", "aionnet", "Rrio1003","50.6.199.166"
 
 (async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Conexión establecida correctamente.");
-  } catch (error) {
-    console.error("Error de conexión:", error);
-  }
+	try {
+		await sequelize.authenticate();
+		console.log("Conexión establecida correctamente.");
+	} catch (error) {
+		console.error("Error de conexión:", error);
+	}
 })();
 
 module.exports = sequelize;
