ac6e3cc185274b917a1c1c595d3da5d660736a89
omararaujo2022 <jair.orozco2000@gmail.com>
2026-05-07 14:13:44 -0600

    Hotfix papeleta
    Cambio de campo puesto en papeleta

--- src/components/Gente & Cultura Permission/Components/tabla_permisos_faltas_usuario.jsx
+++ src/components/Gente & Cultura Permission/Components/tabla_permisos_faltas_usuario.jsx
@@ -1218,7 +1218,7 @@ export function TablaPermisosFaltaUsuario() {
 											Llegada tarde / Salida antes
 										</CardTitle>
 										<DialogDescription className="text-center">
-											Autorización para llegar tarde o salir tempranooo
+											Autorización para llegar tarde o salir tempranox
 										</DialogDescription>
 									</CardHeader>
 									<form onSubmit={handleSubmit}>
